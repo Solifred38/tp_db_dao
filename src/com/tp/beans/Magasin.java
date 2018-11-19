ae675bd3f3f3f75eb33f7501a5e5e27d0bf6cb94
frederic fromager <fred.fromager@gmail.com>
2018-11-19 12:18:25 +0100

    update bean magazin + jsp

---
+++ src/com/tp/beans/Magasin.java
@@ -0,0 +1,26 @@
+package com.tp.beans;
+
+public class Magasin {
+	private String nom;
+	private String description;
+	private int idMagasin;
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getIdMagasin() {
+		return idMagasin;
+	}
+	public void setIdMagasin(int idMagasin) {
+		this.idMagasin = idMagasin;
+	}
+
+}
